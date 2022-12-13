e716126b04ca7ef3fe5faae3ce958248519f9c09
as-bowley <as.bowley@gmail.com>
2022-12-13 20:47:45 +0100

    add upper display and revise logic

--- main.js
+++ main.js
@@ -1,83 +1,33 @@
-const buttons = document.querySelector(".button-container");
-const upperDisplay = document.querySelector(".upper-display");
-const lowDisplay = document.querySelector(".lower-display");
-const add = document.querySelector("#addition");
-const sub = document.querySelector("#subtract");
-const multi = document.querySelector("#multiply");
-const divi = document.querySelector("#divide");
-const equals = document.querySelector("#equals");
-const clear = document.querySelector(".clear");
-const backspaceButton = document.querySelector(".backspace");
-const decimalButton = document.querySelector(".decimal");
-
-buttons.addEventListener("click", input, false);
-clear.addEventListener("click", clearFun, false);
-backspaceButton.addEventListener("click", backspace, false);
-decimalButton.addEventListener("click", decimal, false);
-document.addEventListener("keydown", keyInput, false);
-
 let numValArr = [];
-let operArr = [];
-let prevOperand = "";
-let currOperand = "";
+let operand = "";
 let val1 = 0;
 let val2 = 0;
 let result = 0;
 
-function input(e) {
-  if (e.target.className == "num") {
-    let clickedItem = e.target.id;
-    numValArr.push(clickedItem);
-    lowDisplay.textContent = numValArr.join("");
-    updateUpperDisplay(clickedItem);
-  } else if (e.target.className == "operand") {
-    numCheck();
-    if (prevOperand === "") {
-      let clickedItem = e.target.textContent;
-      prevOperand = clickedItem;
-      updateUpperDisplay(clickedItem);
-    } else if (prevOperand !== "" && currOperand !== "") {
-      let clickedItem = e.target.textContent;
-      prevOperand = currOperand;
-      currOperand = clickedItem;
-      updateUpperDisplay(clickedItem);
-    } else {
-      let clickedItem = e.target.textContent;
-      currOperand = clickedItem;
-      operate(prevOperand, val1, val2);
-      updateUpperDisplay(clickedItem);
-    }
-  } else if (e.target.className === "operate") {
-    numCheck();
-    operate(prevOperand, val1, val2);
-  }
-}
+const buttons = document.querySelector(".button-container");
+const numberButtons = document.querySelectorAll(".num");
+const operandButtons = document.querySelectorAll(".operand");
+const operateButton = document.querySelector(".operate");
+const upperDisplay = document.querySelector(".upper-display");
+const lowDisplay = document.querySelector(".lower-display");
+const clearButton = document.querySelector(".clear");
+const backspaceButton = document.querySelector(".backspace");
+const decimalButton = document.querySelector(".decimal");
 
-function setOperand() {
-  prevOperand = currOperand;
-  currOperand = "";
-}
-
-function setResult(num) {
-  if (String(num).includes(".")) {
-    result = parseFloat(num).toFixed(2);
-    handleResult();
-  } else {
-    result = num;
-    handleResult();
-  }
-}
-
-function handleResult() {
-  lowDisplay.textContent = result;
-  if (result === "!nice try") {
-    val1 = 0;
-  } else {
-    val1 = result;
-  }
-  result = 0;
-  val2 = 0;
-}
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => addNumber(button.textContent));
+});
+operandButtons.forEach((button) => {
+  button.addEventListener("click", () => addOperand(button.textContent));
+});
+operateButton.addEventListener("click", () => {
+  handleValues();
+  operate(operand, val1, val2);
+});
+clearButton.addEventListener("click", clear, false);
+backspaceButton.addEventListener("click", backspace, false);
+decimalButton.addEventListener("click", addDecimal, false);
+document.addEventListener("keydown", keyboardInput, false);
 
 function addition(num1, num2) {
   return num1 + num2;
@@ -103,83 +53,146 @@ function remainder(num1, num2) {
   if (num1 == 0 && num2 == 0) {
     lowDisplay.textContent = "!nice try";
   } else {
-    result = num1 % num2;
-    handleResult();
-  }
-}
-
-function numCheck() {
-  if (val1 === 0 && numValArr.length > 0) {
-    val1 = numValArr.join("");
-    numValArr = [];
-  } else if (numValArr.length > 0) {
-    val2 = numValArr.join("");
-    numValArr = [];
+    return num1 % num2;
   }
 }
 
 function operate(string, num1, num2) {
+  console.log(string, num1, num2);
   switch (string) {
     case "+":
-      setOperand();
+      operand = "";
       setResult(addition(+num1, +num2));
       break;
     case "-":
-      setOperand();
+      operand = "";
       setResult(subtract(+num1, +num2));
       break;
     case "÷":
-      setOperand();
+      operand = "";
       setResult(divide(+num1, +num2));
       break;
     case "x":
-      setOperand();
+      operand = "";
       setResult(multiply(+num1, +num2));
       break;
     case "%":
-      remainder(+num1, +num2);
+      operand = "";
+      setResult(remainder(+num1, +num2));
       break;
     default:
       break;
   }
 }
 
-function clearFun() {
+function addNumber(number) {
+  numValArr.push(number);
+  lowDisplay.textContent = numValArr.join("");
+  updateUpperDisplay(number);
+}
+
+function addOperand(clickedOperand) {
+  if (lowDisplay.textContent !== 0) {
+    if (operand === "") {
+      operand = clickedOperand;
+      updateUpperDisplay(operand);
+    } else {
+      handleValues();
+      operate(operand, val1, val2);
+      operand = clickedOperand;
+      updateUpperDisplay(operand);
+    }
+  }
+  handleValues();
+}
+
+function addDecimal() {
+  if (!numValArr.includes(".")) {
+    numValArr.push(".");
+    lowDisplay.textContent = numValArr.join("");
+    upperDisplay.textContent += ".";
+  }
+}
+
+function clear() {
   val1 = 0;
   val2 = 0;
   numValArr = [];
-  prevOperand = "";
-  currOperand = "";
+  operand = "";
   lowDisplay.textContent = "0";
   upperDisplay.textContent = "";
 }
 
 function backspace() {
-  // delete from upper display & prevent another operand from being pressed
   if (numValArr.length > 0) {
     numValArr.pop();
     lowDisplay.textContent = numValArr.join("");
-    if (numValArr.length == 0) {
-      lowDisplay.textContent = 0;
-    }
+    upperDisplay.textContent = upperDisplay.textContent.slice(0, -1);
+  }
+  if (numValArr.length === 0) {
+    lowDisplay.textContent = 0;
+    upperDisplay.textContent = "";
   }
 }
 
-function decimal() {
-  if (!numValArr.includes(".")) {
-    numValArr.push(".");
-    lowDisplay.textContent = numValArr.join("");
+function setResult(num) {
+  if (String(num).includes(".")) {
+    result = parseFloat(num).toFixed(2);
+    handleResult();
+  } else {
+    result = num;
+    handleResult();
   }
 }
 
-function keyInput(e) {
-  console.log(e.key);
+function handleResult() {
+  lowDisplay.textContent = result;
+  if (result === "!nice try") {
+    val1 = 0;
+  }
+  upperDisplay.textContent = result;
+  val1 = result;
+  result = 0;
+  val2 = 0;
+}
+
+function handleValues() {
+  if (val1 === 0 && numValArr.length > 0) {
+    val1 = numValArr.join("");
+    numValArr = [];
+  } else if (val1 !== 0) {
+    val2 = numValArr.join("");
+    numValArr = [];
+  }
+}
+
+function keyboardInput(event) {
+  const { key } = event;
+  const operands = ["+", "-", "%"];
+
+  if (key >= 0 && +key <= 9) {
+    addNumber(key);
+  } else if (key == ".") {
+    addDecimal();
+  } else if (operands.includes(key)) {
+    addOperand(key);
+  } else if (key == "*") {
+    addOperand("x");
+  } else if (key == "/") {
+    addOperand("÷");
+  } else if (key == "=" || key == "Enter") {
+    handleValues();
+    operate(prevOperand, val1, val2);
+  } else if (key == "Backspace") {
+    backspace();
+  }
 }
 
 function updateUpperDisplay(item) {
   let lastItemIsOperand = false;
   const displayArray = upperDisplay.textContent.split("");
   const operandArray = ["x", "+", "-", "%", "/"];
+
   if (operandArray.includes(item)) {
     operandArray.forEach((operand) => {
       if (operand == displayArray[displayArray.length - 1]) {
